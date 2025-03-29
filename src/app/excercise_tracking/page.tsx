9fce2212a716999052a61d8bade6ff02d8c0b9a8
Sharif15 <Sharifpial225@gmail.com>
2025-03-29 13:52:46 -0600

    set up initial layout for the website routes

---
+++ src/app/excercise_tracking/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function ExcerciseTracking() {
+  return (
+    <div>
+      <main>
+        <p>Temp text</p>
+      </main>
+      <footer>
+        
+      </footer>
+    </div>
+  );
+}
---
+++ src/app/logs/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Logs() {
+  return (
+    <div>
+      <main>
+        <p>Temp text</p>
+      </main>
+      <footer>
+        
+      </footer>
+    </div>
+  );
+}
